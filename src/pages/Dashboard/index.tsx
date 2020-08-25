c8355c276ed2dca59a580e23cb48b2d12ba05c4f
Gabriel Porto <gabriel_porto@outlook.com>
2020-08-25 01:40:58 -0300

    add: dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,11 @@
 import React, { useState, useRef, useCallback } from 'react';
-import Select from 'react-select';
+import {
+  FiTerminal,
+  FiLinkedin,
+  FiMail,
+  FiUser,
+  FiCalendar,
+} from 'react-icons/fi';
 
 import { Form } from '@unform/web';
 
@@ -7,6 +13,7 @@ import { FormHandles } from '@unform/core';
 import Modal from '../../components/Modal';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import Select from '../../components/Select';
 
 import { Container } from './styles';
 
@@ -15,28 +22,55 @@ const Dashboard: React.FC = () => {
 
   const [modalOpen, setModalOpen] = useState(true);
 
-  const handleSubmit = useCallback((data: any) => {}, []);
+  const handleSubmit = useCallback((data: any) => {
+    console.log(data);
+  }, []);
 
   const toggleModal = useCallback(() => {
     setModalOpen(!modalOpen);
   }, [modalOpen]);
 
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
+
   return (
-    <Container>
-      <Modal isOpen={modalOpen} setIsOpen={toggleModal}>
+    <Modal isOpen={modalOpen} setIsOpen={toggleModal}>
+      <Container>
+        <h1>Cadastrar</h1>
         <Form
           ref={formRef}
           onSubmit={handleSubmit}
           style={{ width: '85%', margin: 'auto' }}
         >
-          <Input type="text" name="name" placeholder="Nome" />
-          <Input type="email" name="email" placeholder="E-mail" />
-          <Input type="number" name="age" placeholder="Idade" />
-          <Input type="text" name="linkedin" placeholder="Linkedin" />
+          <Input type="text" name="name" icon={FiUser} placeholder="Nome" />
+          <Input type="email" name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            type="number"
+            name="age"
+            icon={FiCalendar}
+            placeholder="Idade"
+          />
+          <Input
+            type="text"
+            name="linkedin"
+            icon={FiLinkedin}
+            placeholder="Linkedin"
+          />
+
+          <Select
+            name="techs"
+            icon={FiTerminal}
+            options={options}
+            placeholder="Tecnologias"
+            isMulti
+          />
           <Button type="submit">Salvar</Button>
         </Form>
-      </Modal>
-    </Container>
+      </Container>
+    </Modal>
   );
 };
 
