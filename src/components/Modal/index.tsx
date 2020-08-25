58c05d0809bb30e2f730f6036fc8833e2ee7ded1
Gabriel Porto <gabriel_porto@outlook.com>
2020-08-24 23:21:02 -0300

    add: modal

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+
+import ReactModal from 'react-modal';
+
+interface IModalProps {
+  children: any;
+  isOpen: boolean;
+  setIsOpen: () => void;
+}
+
+const Modal: React.FC<IModalProps> = ({ children, isOpen, setIsOpen }) => {
+  const [modalStatus, setModalStatus] = useState(isOpen);
+
+  useEffect(() => {
+    setModalStatus(isOpen);
+  }, [isOpen]);
+
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick
+      onRequestClose={setIsOpen}
+      isOpen={modalStatus}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          background: '#fff',
+          color: '#000000',
+          borderRadius: '8px',
+          width: '75%',
+          border: 'none',
+        },
+        overlay: {
+          backgroundColor: '#121214e6',
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+};
+
+export default Modal;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState, useRef, useCallback } from 'react';
+import Select from 'react-select';
+
+import { Form } from '@unform/web';
+
+import { FormHandles } from '@unform/core';
+import Modal from '../../components/Modal';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const [modalOpen, setModalOpen] = useState(true);
+
+  const handleSubmit = useCallback((data: any) => {}, []);
+
+  const toggleModal = useCallback(() => {
+    setModalOpen(!modalOpen);
+  }, [modalOpen]);
+
   return (
     <Container>
-      <h1>Hello World</h1>
+      <Modal isOpen={modalOpen} setIsOpen={toggleModal}>
+        <Form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          style={{ width: '85%', margin: 'auto' }}
+        >
+          <Input type="text" name="name" placeholder="Nome" />
+          <Input type="email" name="email" placeholder="E-mail" />
+          <Input type="number" name="age" placeholder="Idade" />
+          <Input type="text" name="linkedin" placeholder="Linkedin" />
+          <Button type="submit">Salvar</Button>
+        </Form>
+      </Modal>
     </Container>
   );
 };
