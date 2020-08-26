5f91528e6b098e880d45bc33e702dc1970ea3ba3
Gabriel Porto <gabriel_porto@outlook.com>
2020-08-25 21:47:28 -0300

    feat: dashboard listing

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,19 +1,175 @@
-import React, { useCallback } from 'react';
-import { useCreateCandidate } from '../../hooks/CreateCandidate';
+import React, { useCallback, useState, useMemo, useEffect } from 'react';
+import { Form } from '@unform/web';
+
+import {
+  FiTrash2,
+  FiEdit3,
+  FiMail,
+  FiLinkedin,
+  FiCalendar,
+  FiTerminal,
+} from 'react-icons/fi';
 
 import Button from '../../components/Button';
+import Select from '../../components/Select';
+
+import { useCreateCandidate } from '../../hooks/CreateCandidate';
+
+import { Container, CandidateItens, Header } from './styles';
+import api from '../../services/api';
+
+type Techs =
+  | 'C#'
+  | 'Javascript'
+  | 'Nodejs'
+  | 'Angular'
+  | 'React'
+  | 'Ionic'
+  | 'Mensageria'
+  | 'PHP'
+  | 'Laravel';
+
+interface ICandidateDTO {
+  id: string;
+  name: string;
+  email: string;
+  age: number;
+  linkedin: string;
+  techs: Techs[];
+}
+
+interface iOption {
+  label: string;
+  value: Techs;
+}
 
 const Dashboard: React.FC = () => {
-  const { openCreateCandidate } = useCreateCandidate();
+  const { openCreateCandidate, isOpenCreateCandidate } = useCreateCandidate();
 
-  const open = useCallback(() => {
+  const [searchSelected, setSearchSelected] = useState<Techs[]>([]);
+  const [candidatesList, setCandidatesList] = useState<ICandidateDTO[]>([]);
+
+  useEffect(() => {
+    async function loadCandidates(): Promise<void> {
+      const candidates = await api.get<ICandidateDTO[]>('/candidates');
+      const { data } = candidates;
+      setCandidatesList([...data]);
+    }
+    loadCandidates();
+  }, [isOpenCreateCandidate]);
+
+  const openModal = useCallback(() => {
     openCreateCandidate();
   }, [openCreateCandidate]);
 
+  const handleSelect = useCallback(
+    (e) => {
+      const eventValue = e as iOption[];
+      if (eventValue) {
+        const value = eventValue.map((selectMap) => selectMap.value || '');
+        setSearchSelected([...value]);
+      }
+    },
+    [setSearchSelected],
+  );
+
+  const candidatesFiltered = useMemo(() => {
+    const filtered = candidatesList.filter((candidateToBeFilter) => {
+      return searchSelected.every((v) => candidateToBeFilter.techs.includes(v));
+    });
+    return filtered;
+  }, [candidatesList, searchSelected]);
+
+  const handleDelete = useCallback(
+    async (id) => {
+      await api.delete(`/candidates/${id}`);
+      const newList = candidatesList.filter((item) => item.id !== id);
+      setCandidatesList(newList);
+    },
+    [candidatesList],
+  );
+
+  const options = [
+    { value: 'C#', label: 'C#' },
+    { value: 'Javascript', label: 'Javascript' },
+    { value: 'Angular', label: 'Angular' },
+    { value: 'React#', label: 'React' },
+    { value: 'Ionic', label: 'Ionic' },
+    { value: 'Mensageria', label: 'Mensageria' },
+    { value: 'PHP', label: 'PHP' },
+    { value: 'Laravel', label: 'Laravel' },
+  ];
+
   return (
-    <Button type="button" onClick={open}>
-      teste
-    </Button>
+    <Container>
+      <Header>
+        <nav>vaggo</nav>
+        <Form onSubmit={() => null}>
+          <menu>
+            <strong>Candidatos disponíveis</strong>
+            <Button type="button" onClick={openModal}>
+              Criar Novo
+            </Button>
+          </menu>
+
+          <Select
+            name="techs"
+            icon={FiTerminal}
+            options={options}
+            placeholder="Tecnologias"
+            onChange={handleSelect}
+            isMulti
+          />
+        </Form>
+      </Header>
+      <main>
+        {candidatesFiltered.map((candidateFiltered) => (
+          <CandidateItens key={candidateFiltered.id}>
+            <header>
+              <strong>{candidateFiltered.name}</strong>
+              <div>
+                {candidateFiltered.techs.map((tech) => (
+                  <span key={tech}>{tech}</span>
+                ))}
+              </div>
+            </header>
+            <section>
+              <p>
+                <FiMail size={20} />
+                {candidateFiltered.email}
+              </p>
+              <p>
+                <FiCalendar size={20} />
+                {candidateFiltered.age}
+              </p>
+              <p>
+                <FiLinkedin size={20} />
+                {candidateFiltered.linkedin}
+              </p>
+            </section>
+            <footer>
+              <Button
+                type="button"
+                onClick={() => {
+                  handleDelete(candidateFiltered.id);
+                }}
+              >
+                <div>
+                  <FiTrash2 />
+                </div>
+                <p>Deletar</p>
+              </Button>
+              <Button type="button">
+                <div>
+                  <FiEdit3 />
+                </div>
+                <p>Editar</p>
+              </Button>
+            </footer>
+          </CandidateItens>
+        ))}
+      </main>
+    </Container>
   );
 };
 
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,3 +1,131 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  main {
+    display: grid;
+    grid-template-columns: 1fr;
+    margin: 3.2rem auto;
+    width: 80%;
+  }
+`;
+
+export const Header = styled.header`
+  display: flex;
+  flex-direction: column;
+  background-color: #1c56ac;
+  height: 275px;
+
+  nav {
+    text-align: center;
+    color: #fff;
+    font-size: 32px;
+    font-weight: 700;
+  }
+
+  form {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 2fr;
+    margin: 3.2rem auto;
+    width: 80%;
+
+    position: relative;
+    menu {
+      display: flex;
+
+      strong {
+        flex: 2;
+        font-weight: 700;
+        font-size: 3em;
+        color: #fff;
+      }
+
+      button {
+        flex: 1;
+        background: #fff;
+        color: #1c56ac;
+        margin-left: 10px;
+      }
+    }
+
+    > div {
+      position: absolute;
+      top: 160px;
+    }
+  }
+`;
+
+export const CandidateItens = styled.article`
+  background: #fff;
+  border: 1px solid #e6e6f0;
+  border-radius: 0.8rem;
+  margin-top: 2.4rem;
+  overflow: hidden;
+
+  header {
+    padding: 1rem 2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: #fafafc;
+    border-bottom: 1px solid #e6e6f0;
+
+    strong {
+      font-size: 42px;
+      font-weight: 300;
+    }
+
+    div {
+      display: flex;
+
+      span {
+        background: #1c56ac;
+        color: #fff;
+        padding: 10px;
+        margin: 10px;
+        border-radius: 5px;
+      }
+    }
+  }
+
+  section {
+    padding: 0 2rem;
+    font-size: 1.6rem;
+    line-height: 2.8rem;
+    margin: 1rem 0;
+
+    p {
+      svg {
+        margin-right: 10px;
+      }
+    }
+  }
+
+  footer {
+    padding: 1rem 2rem;
+    background: #fafafc;
+    border-top: 1px solid #e6e6f0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    button {
+      display: flex;
+      align-items: center;
+      margin-top: 0;
+      margin: 0 5px;
+      div {
+        flex: 1;
+        svg {
+          margin-right: 15px;
+        }
+      }
+
+      p {
+        flex: 2;
+      }
+
+      width: 10rem;
+    }
+  }
+`;
